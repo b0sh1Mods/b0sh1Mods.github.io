4c11c1b26b03599899bd06ffd830e2c4ba161888
Boshi <157933924+b0sh1Mods@users.noreply.github.com>
2025-07-17 00:32:07 -0400

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,19 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("darkmode");
+let darkmode = localStorage.getItem('darkmode')
+const themeswitch = document.getElementById('theme-switch')
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
 }
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode == "active") enableDarkMode()
+
+themeswitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
